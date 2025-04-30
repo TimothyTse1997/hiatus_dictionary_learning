aa3aecea7c47b62c5b6e3e7e464fd801c375b3c2
Tin Long Tse <tltse@narval2.narval.calcul.quebec>
2025-04-30 04:53:12 -0400

    remove debug comment

--- multi_generate_and_score_ta1.py
+++ multi_generate_and_score_ta1.py
@@ -31,58 +31,58 @@ if __name__ == "__main__":
     args = parser.parse_args()
     all_generated_file = []
 
-    #for model_path in Path(args.dict_learning_model_dir).glob("**/ae*.pt"):
-    #    parent_dir = model_path.parent
-    #    if bool(re.search("ae\_[0-9]+\.pt", model_path.name)):
-    #        parent_dir = model_path.parent.parent
-    #        checkpoint_step = int(re.split("\_|\.", model_path.name)[1])
+    for model_path in Path(args.dict_learning_model_dir).glob("**/ae*.pt"):
+        parent_dir = model_path.parent
+        if bool(re.search("ae\_[0-9]+\.pt", model_path.name)):
+            parent_dir = model_path.parent.parent
+            checkpoint_step = int(re.split("\_|\.", model_path.name)[1])
 
-    #        sparse_output_file = parent_dir / f"eval_embed_{checkpoint_step}.sparse.jsonl"
-    #        recon_output_file = parent_dir / f"eval_embed_{checkpoint_step}.recon.jsonl"
+            sparse_output_file = parent_dir / f"eval_embed_{checkpoint_step}.sparse.jsonl"
+            recon_output_file = parent_dir / f"eval_embed_{checkpoint_step}.recon.jsonl"
 
-    #        sparse_score_ourput_dir = parent_dir/f"ta1_sparse_score_{checkpoint_step}_result" 
-    #        recon_score_ourput_dir = parent_dir/f"ta1_recon_score_{checkpoint_step}_result" 
-    #    else:
-    #        sparse_output_file = parent_dir / "eval_embed.sparse.jsonl"
-    #        recon_output_file = parent_dir / "eval_embed.recon.jsonl"
+            sparse_score_ourput_dir = parent_dir/f"ta1_sparse_score_{checkpoint_step}_result" 
+            recon_score_ourput_dir = parent_dir/f"ta1_recon_score_{checkpoint_step}_result" 
+        else:
+            sparse_output_file = parent_dir / "eval_embed.sparse.jsonl"
+            recon_output_file = parent_dir / "eval_embed.recon.jsonl"
 
-    #        sparse_score_ourput_dir = parent_dir/ "ta1_sparse_score_result" 
-    #        recon_score_ourput_dir = parent_dir/ "ta1_recon_score_result" 
-    #    if recon_output_file.exists() and len(list(recon_score_ourput_dir.glob("*"))):
-    #        continue
-    #    run_single_checkpoint_generation(
-    #        str(model_path), 
-    #        "/project/def-lingjzhu/tltse/official_data/english.preds.jsonl",
-    #        [recon_output_file, sparse_output_file]
-    #    )
-    #    print("finish generation")
-    #    print(sparse_output_file)
-    #    print(recon_output_file)
-    #    print(model_path)
-    #    print(parent_dir)
-    #    print("sparse scoring")
-    #    all_generated_file += [sparse_output_file, recon_output_file]
-    #    subprocess.run(
-    #        [
-    #            "/project/def-lingjzhu/tltse/ir-aa-master/scripts/score_ta1.sh",
-    #            str(sparse_output_file),
-    #            str(args.eval_dataset),
-    #            "english",
-    #            str(sparse_score_ourput_dir),
-    #            "hrs2"
-    #        ]
-    #    )
-    #    print("recon scoring")
-    #    subprocess.run(
-    #        [
-    #            "/project/def-lingjzhu/tltse/ir-aa-master/scripts/score_ta1.sh",
-    #            str(recon_output_file),
-    #            str(args.eval_dataset),
-    #            "english",
-    #            str(recon_score_ourput_dir),
-    #            "hrs2"
-    #        ]
-    #    )
+            sparse_score_ourput_dir = parent_dir/ "ta1_sparse_score_result" 
+            recon_score_ourput_dir = parent_dir/ "ta1_recon_score_result" 
+        if recon_output_file.exists() and len(list(recon_score_ourput_dir.glob("*"))):
+            continue
+        run_single_checkpoint_generation(
+            str(model_path), 
+            "/project/def-lingjzhu/tltse/official_data/english.preds.jsonl",
+            [recon_output_file, sparse_output_file]
+        )
+        print("finish generation")
+        print(sparse_output_file)
+        print(recon_output_file)
+        print(model_path)
+        print(parent_dir)
+        print("sparse scoring")
+        all_generated_file += [sparse_output_file, recon_output_file]
+        subprocess.run(
+            [
+                "/project/def-lingjzhu/tltse/ir-aa-master/scripts/score_ta1.sh",
+                str(sparse_output_file),
+                str(args.eval_dataset),
+                "english",
+                str(sparse_score_ourput_dir),
+                "hrs2"
+            ]
+        )
+        print("recon scoring")
+        subprocess.run(
+            [
+                "/project/def-lingjzhu/tltse/ir-aa-master/scripts/score_ta1.sh",
+                str(recon_output_file),
+                str(args.eval_dataset),
+                "english",
+                str(recon_score_ourput_dir),
+                "hrs2"
+            ]
+        )
     
     
     print("now run statistic for all data (explained varience)")
